709ffc53b9552e23127ef0bbd8c8f43eca0655e2
Ishnika <ishnika25csu091@ncuindia.edu>
2026-02-06 13:44:06 +0530

    Implement area calculation for circular fountain

---
+++ area_fountain.py
@@ -0,0 +1,5 @@
+"""An architect wants to calculate the space covered by a circular fountain. Implement a solution to compute
+the area of a circle."""
+rad=int(input("Enter the radius of the fountain:"))
+ar=3.14*(rad**2)
+print("The area of the fountaion is:",ar)
