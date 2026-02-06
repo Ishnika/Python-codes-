dc36e3b2d5420459a162fdccbfe77551bd6f8085
Ishnika <ishnika25csu091@ncuindia.edu>
2026-02-06 13:40:02 +0530

    Implement average marks calculation for three students

---
+++ avg_marks.py
@@ -0,0 +1,8 @@
+"""A teacher wants to calculate the average marks of three students to determine the class performance.
+Implement a solution to accept three numbers and compute their average."""
+m1=int(input("enter the marks of first student:"))
+m2=int(input("enter the marks of the second student:"))
+m3=int(input("enter the marks of the third student:"))
+sum=m1+m2+m3
+avg=sum/3
+print("the average marks are:",avg)
