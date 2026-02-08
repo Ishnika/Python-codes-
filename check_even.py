f20ee01ebd2fcee1da8d45980a647d87c25c3830
Ishnika <ishnika25csu091@ncuindia.edu>
2026-02-08 18:06:39 +0530

    Add function to check if a number is even

---
+++ check_even.py
@@ -0,0 +1,8 @@
+#check if the given number is even or not 
+def is_even(n):
+    if n%2==0 :
+        return True 
+    else :
+        return False 
+n=int(input("Enter the number:"))
+print(is_even(n))
